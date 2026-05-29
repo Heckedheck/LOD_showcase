d2b3167aa1995266bbca100b906bf8b915f417d8
Heckedheck <145862335+Heckedheck@users.noreply.github.com>
2026-05-29 17:15:14 +0200

    movemmgdfb

--- LOD showcase/Assets/playershit/Player.cs
+++ LOD showcase/Assets/playershit/Player.cs
@@ -3,18 +3,60 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody rb;
-    [SerializeField] private Vector2 Movement;
+    [SerializeField] private InputActionAsset inputActions;
+    private InputAction move;
+    private InputAction look;
+    
+    private Vector2 movement;
+    private Vector2 looking;
 
-    private void Start()
+    private Rigidbody rb;
+
+    public float moveSpeed = 5.0f;
+    public float lookSpeed = 5.0f;
+
+    private void OnEnable()
     {
+        inputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        move = InputSystem.actions.FindAction("Move");
+        look = InputSystem.actions.FindAction("Look");
+
         rb = GetComponent<Rigidbody>();
     }
-    public void OnMove(InputValue value)
+
+    private void Update()
     {
-        // Read value from control. The type depends on what type of controls.
-        // the action is bound to.
-        Movement = value.Get<Vector2>();
-        rb.AddForce(Movement);
+        movement = move.ReadValue<Vector2>();
+        looking = look.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Looking();
+    }
+
+    private void Walking()
+    {
+        rb.MovePosition(rb.position + transform.forward * movement.y * moveSpeed * Time.deltaTime);
+    }
+
+    private void Looking()
+    {
+        if (movement.y != 0)
+        {
+            float rotationAmount = looking.x * lookSpeed * Time.deltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(0, rotationAmount, 0);
+            rb.MoveRotation(rb.rotation *  deltaRotation);
+        }
     }
 }
